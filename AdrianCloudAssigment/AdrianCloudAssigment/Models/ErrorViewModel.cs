b1be17effc7fce46eab866f546069e5c7037d123
AdrianPortelli <adrian.portelli.c10042@mcast.edu.mt>
2022-04-01 12:01:49 +0200

    Project Set up and Google Login and Log Out

---
+++ AdrianCloudAssigment/AdrianCloudAssigment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdrianCloudAssigment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
